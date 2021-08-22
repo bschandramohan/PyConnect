b9b39315e3d9973af8c7cf901d8067197366862a
cbs <chandra_mohan@intuit.com>
2021-08-22 16:07:03 -0700

    Multiple if else conditions

---
+++ IsLeapYear.py
@@ -0,0 +1,12 @@
+year = int(input("Enter year to check for leap year:\n"))
+print(f"finding if {year} is leap year")
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year")
+        else:
+            print("Not Leap Year")
+    else:
+        print("Not Leap Year")
+else:
+    print ("Not leap year")
