2812a08194fd24d6c95a8c521b40498bc17d090a
bschandramohan <bschandramohan@gmail.com>
2021-08-30 19:31:56 -0700

    More specific example of selective list creation from existing list

--- learn/list_comprehesions.py
+++ learn/list_comprehesions.py
@@ -4,3 +4,10 @@ evens = [i for i in range(20) if not i % 2]
 
 print(squares)
 print(evens)
+
+
+numbers_list = [1, 3, 5, 6, 8, 11, 15, 20, 21, 29]
+
+multiples_of3_list = [number for number in numbers_list if number % 3 == 0]
+print(multiples_of3_list)
+
