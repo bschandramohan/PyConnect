f970d8981b0f23e26308500e93d8441abd478f26
bschandramohan <bschandramohan@gmail.com>
2021-09-08 13:17:38 -0700

    * and ** parameters for functions

---
+++ learn/advanced_parameters.py
@@ -0,0 +1,27 @@
+def sum_parameters(*args):
+    # print(type(args))
+    total = 0
+    for value in args:
+        total += value
+    return total
+
+
+print(sum_parameters(1, 4, 6, 9))
+
+
+def calculate(n, **operations):
+    result = n
+    result += operations.get("add")
+    result *= operations.get("multiply")
+    return result
+
+
+print(calculate(5, add=10, multiply=15))
+# TODO: Investigate how to ignore operation if get returns NONE
+# TypeError: unsupported operand type(s) for *=: 'int' and 'NoneType'
+# print(calculate(5, add=10))
+# print(calculate(5, multiply=15))
+
+# NOTE: Keys cannot be repeated in  keywordarguments a.k.a **kwargs
+# print(calculate(5, add=10, multiply=15, add=5))
+# SyntaxError: keyword argument repeated: add
