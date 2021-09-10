e057924a10ac583b9e5da1caf36a04b3b9fa3a8a
bschandramohan <bschandramohan@gmail.com>
2021-09-10 11:15:05 -0700

    Fixing PyCharm warnings

--- learn/hangman.py
+++ learn/hangman.py
@@ -1,11 +1,14 @@
 import random
 
 a = 3
+
+
 def try_fun():
     if a > 2:
         b = 1
     print(f"{a} {b}")
-    
+
+
 try_fun()
 
 print("Hangman game")
@@ -15,11 +18,13 @@ word_list = ["amazing", "harry", "movie", "showdown"]
 
 word_chosen = random.choice(word_list)
 
-def printCharacterList(list):
+
+def print_character_list(list):
     str = ""
     for index in range(len(list)):
         str += list[index] + " "
     print(str + "\n")
+
 
 # print word with underlines
 word_underline = []
@@ -32,23 +37,23 @@ guessed_characters = []
 
 # using while loop instead of for since we can't change index (for continue cases below) in for
 guesses_index = 0
-while (guesses_index < total_lives):
-    printCharacterList(word_underline)
+while guesses_index < total_lives:
+    print_character_list(word_underline)
     character = input(f"{guesses_index}: Guess the character: ").lower()
     if len(character) > 1:
         print("Invalid entry")
         continue
-    
+
     if not character.isalnum():
         print("Not alpha numeric character")
         continue
-    
+
     if guessed_characters.count(character) > 0:
         print("You already guessed the character. Try something else")
         continue
-    
+
     guessed_characters += character
-    
+
     present_count = word_chosen.count(character)
     if present_count > 0:
         print("Good guess")
@@ -57,7 +62,7 @@ while (guesses_index < total_lives):
         for position in range(len(word_chosen)):
             if word_chosen[position].lower() == character.lower():
                 word_underline[position] = character
-        
+
         if correct_guesses_count == len(word_chosen):
             print("Correctly guessed the word")
             break
@@ -65,6 +70,6 @@ while (guesses_index < total_lives):
         guesses_index += 1
         print("Sorry! Not present")
 
-print (f"\nWord chosen was {word_chosen}\n"
-       f"You guessed correctly {correct_guesses_count} character(s) in the word.\n"
-       f"Characters guessed correctly were {correct_characters}\n")
+print(f"\nWord chosen was {word_chosen}\n"
+      f"You guessed correctly {correct_guesses_count} character(s) in the word.\n"
+      f"Characters guessed correctly were {correct_characters}\n")
