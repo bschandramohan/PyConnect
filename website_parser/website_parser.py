6a1f2fcd21299b179f379c504d1f4b9c9bf693bf
bschandramohan <bschandramohan@gmail.com>
2021-09-14 19:38:53 -0700

    Using List composition to create aeticle titles list

--- website_parser/website_parser.py
+++ website_parser/website_parser.py
@@ -8,12 +8,16 @@ response = requests.get("https://medium.com/techieconnect")
 
 soup = BeautifulSoup(response.text, "html.parser")
 article_title = soup.find(name="h1", class_="js-collectionName")
-print("Blog Name:", article_title.getText, "\n")
+print("Blog Name:", article_title.getText(), "\n")
 
 articles_list = soup.find_all(class_="u-breakWord")
 # articles_list = soup.select(selector="js-collectionName")
 # print(articles_list)
 
-for i in range(len(articles_list)):
-    article = articles_list[i]
-    print(f"{i+1}. {article.getText()}")
+# for i in range(len(articles_list)):
+#     article = articles_list[i]
+#     print(f"{i+1}. {article.getText()}")
+
+# Another way
+articles_title = [article.getText() for article in articles_list]
+print(articles_title)
