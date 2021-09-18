ec5adf8eff7e990e5fa4851b18743c1314741b6a
bschandramohan <bschandramohan@gmail.com>
2021-09-18 16:51:53 -0700

    Stack Data Structure implementation

---
+++ data_structures/stack.py
@@ -0,0 +1,53 @@
+
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.previous = None
+
+
+def display(fn):
+    def operation(*args, **kwargs):
+        print(f"Operation={str.upper(fn.__name__)}")
+        fn(*args, **kwargs)
+        current_node = args[0].top
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.previous
+        print("END_OF_STACK\n")
+    return operation
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    @display
+    def push(self, value):
+        new_node = Node(value)
+
+        if self.top is None:
+            self.top = new_node
+        else:
+            new_node.previous = self.top
+            self.top = new_node
+
+    @display
+    def pop(self):
+        if self.top is not None:
+            return_value = self.top.data
+            self.top = self.top.previous
+            return return_value
+        else:
+            return -1
+
+
+stack = Stack()
+stack.pop()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.pop()
+stack.push(4)
+stack.pop()
+stack.pop()
+stack.pop()
